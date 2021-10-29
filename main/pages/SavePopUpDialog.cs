8dd28e77ab566600a0a2a6f4b45a1d1a00e4e5a6
qshogun <55805051+qshogun@users.noreply.github.com>
2021-10-29 13:13:03 +0200

    Create SavePopUpDialog.cs

---
+++ main/pages/SavePopUpDialog.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Appium.Windows;
+
+namespace NotebookTesting
+{
+    public class SavePopUpDialog : BasePage
+    {
+        // Windows Elements visible under File menu
+        public WindowsElement FileNameField => driver.FindElementByAccessibilityId("1001");
+        public WindowsElement SaveButton => driver.FindElementByAccessibilityId("1");
+        public WindowsElement CancelButton => driver.FindElementByAccessibilityId("2");
+
+
+        public SavePopUpDialog(WindowsDriver<WindowsElement> driver)
+            : base(driver)
+        {
+        }
+
+        public SavePopUpDialog TypeFileName(string fileName)
+        {
+            FileNameField.SendKeys(fileName);
+            return this;
+        }
+    }
+}
