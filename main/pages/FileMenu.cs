fa4aeaec4a9a925e0ed1b5fd705b25f626cf0825
qshogun <55805051+qshogun@users.noreply.github.com>
2021-10-29 10:42:23 +0200

    Create FileMenu.cs

---
+++ main/pages/FileMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace NotebookTesting
+{
+    public class FileMenu : BasePage
+    {
+        // Windows Elements visible under File menu
+        public WindowsElement NewFileButton => driver.FindElementByAccessibilityId("1");
+        public WindowsElement NewWindowButton => driver.FindElementByAccessibilityId("8");
+        public WindowsElement OpenButton => driver.FindElementByAccessibilityId("2");
+        public WindowsElement SaveButton => driver.FindElementByAccessibilityId("3");
+        public WindowsElement SaveAsButton => driver.FindElementByAccessibilityId("4");
+        public WindowsElement SettingsButton => driver.FindElementByAccessibilityId("5");
+        public WindowsElement PrintButton => driver.FindElementByAccessibilityId("6");
+        public WindowsElement CloseButton => driver.FindElementByAccessibilityId("7");
+
+        public FileMenu(WindowsDriver<WindowsElement> driver)
+            : base(driver)
+        {
+        }
+    }
+}
