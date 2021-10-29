7736170ada7c5f973540370600bdd61986312f94
qshogun <55805051+qshogun@users.noreply.github.com>
2021-10-29 10:44:37 +0200

    Create Helpers.cs

---
+++ main/utils/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace NotebookTesting
+{
+    public class Helpers
+    {
+        public static Dictionary<string, string> GetData(string filePath)
+        {
+            var data = File
+            .ReadAllLines(filePath)
+            .Select(x => x.Split('='))
+            .Where(x => x.Length > 1)
+            .ToDictionary(x => x[0].Trim(), x => x[1]);
+            return data;
+        }
+    }
+}
