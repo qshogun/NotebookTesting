2b8650edf7f35434c89814b975954a180c88486c
qshogun <55805051+qshogun@users.noreply.github.com>
2021-10-29 10:44:53 +0200

    Create TestSuite.cs

---
+++ test/TestSuite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Faker;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Collections.Generic;
+
+namespace NotebookTesting
+{
+    public class TestSuite
+    {
+        // TODO
+        // private static Dictionary<string, string> setupFile = Helpers.GetData("TestsSetup.txt");
+        // private string _WinAPPDriverServer = setupFile["WinAPPDriverServer"];
+        // private string _ApplicationPathOrAppName = setupFile["ApplicationPathOrAppName"];
+        private const string _WinAPPDriverServer = "http://127.0.0.1:4723";
+        private const string _ApplicationPathOrAppName = @"C:\Windows\System32\notepad.exe";
+        private const int TimeOutForElement = 20;
+        public WindowsDriver<WindowsElement> driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            AppiumOptions appCapabilities = new AppiumOptions();
+            appCapabilities.AddAdditionalCapability("app", _ApplicationPathOrAppName);
+            driver = new WindowsDriver<WindowsElement>(new Uri(_WinAPPDriverServer), appCapabilities);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(TimeOutForElement);
+        }
+    }
+}
